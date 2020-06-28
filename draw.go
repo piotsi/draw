fa0f61e06e42e9ea5e8cf6dfd6a82b66850b54cd
Piotr Sikora <piotrsikoraaa@gmail.com>
2020-06-28 22:31:46 +0200

    Added autosave functionality and ability to display external text

--- draw.go
+++ draw.go
@@ -7,8 +7,8 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-// RecognizedDigit is recognized digit
-var RecognizedDigit int
+// Text can be additional information to display from the outside of package
+var Text string
 
 var (
 	mousePos     rl.Vector2
@@ -17,15 +17,20 @@ var (
 	chosenColor  rl.Color
 	brushSize    float32
 	toClear      bool
+	autoSave     bool
+	autoSaveTime int
+	frame        int
 )
 
 const (
 	windowHeight = 560
 	windowWidth  = 560
+	targetFPS    = 120
 )
 
 // Run runs the application
-func Run() {
+func Run(autosave bool) {
+	autoSave = autosave
 	initialize()
 
 	for !rl.WindowShouldClose() {
@@ -40,23 +45,27 @@ func Run() {
 
 func initialize() {
 	rl.InitWindow(windowWidth, windowHeight, "GoDraw")
-	rl.SetTargetFPS(120)
+	rl.SetTargetFPS(targetFPS)
 
 	// Load clean canvas
 	canvas = rl.LoadRenderTexture(windowWidth, windowHeight)
 	rl.BeginTextureMode(canvas)
 	rl.ClearBackground(rl.White)
 	rl.EndTextureMode()
-
-	brushSize = 30
+	if autoSave {
+		autoSaveTime = targetFPS
+	}
+	autoSaveTime = 1
+	brushSize = 25
 	chosenColor = rl.Black
 }
 
 func update() {
 	mousePos = rl.GetMousePosition()
+	frame++
 
 	// Save texture to png file
-	if rl.IsKeyPressed(rl.KeyS) {
+	if rl.IsKeyPressed(rl.KeyS) || frame%autoSaveTime == 0 {
 		image := rl.GetTextureData(canvas.Texture)
 		rl.ImageFlipVertical(image)
 		rl.ImageColorInvert(image)    // Invert color
@@ -69,12 +78,7 @@ func update() {
 		rl.UnloadImage(image)
 
 		// Clear canvas
-		toClear = true
-	}
-
-	// Run neural network to recognize digit
-	if rl.IsKeyPressed(rl.KeyR) {
-		//RecognizedDigit = NN()
+		// toClear = true
 	}
 
 	// Brush resizing
@@ -115,7 +119,7 @@ func draw() {
 	rl.DrawCircle(int32(mousePos.X), int32(mousePos.Y), brushSize, chosenColor)
 	rl.DrawCircleLines(int32(mousePos.X), int32(mousePos.Y), brushSize, rl.Black)
 	rl.DrawText(fmt.Sprintf("%.f %.f", mousePos.X, mousePos.Y), 20, 20, 20, rl.Black)
-	rl.DrawText(fmt.Sprintf("Recognized digit: %d", RecognizedDigit), 20, 50, 20, rl.Black)
+	rl.DrawText(fmt.Sprintf("%s", Text), 20, 50, 20, rl.Black)
 
 	// Clear the canvas
 	if rl.IsKeyPressed(rl.KeyC) || toClear {
